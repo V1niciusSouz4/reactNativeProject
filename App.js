bcc1faf289b4f24432b3e7963eac366998d9879d
VINÍCIUS SOUZA SANTOS <3343.343@edu.fb.org.br>
2023-08-02 19:10:11 -0300

    TCC project init

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <Text style={styles.title}>Meu primeiro App</Text>
+      <Nome nome="Matheus Fraga"/>
+      <Nome nome="Lucas Silva"/>
+      <Nome nome="Henrique Silva"/>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: 400
   },
-});
+  title:{
+    fontSize: 24,
+    marginTop: 100
+  }
+})
+
+function Nome({nome}){
+  return (
+    <Text>Olá, {nome}</Text>
+  )
+}
